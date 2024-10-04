2c4bb7134197142304a8444f60e9b83767a9d443
Kenny Ek <kennyswek@gmail.com>
2024-10-04 23:41:10 +0200

    feat: Print char count

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 def main():
-  with open("books/frankenstein.txt") as f:
-    text = f.read()
-    print(word_count(text))
+  path = "books/frankenstein.txt"
+  text = get_text(path)
+  print(word_count(text))
+  print(char_count(text))
+
+def get_text(path):
+  with open(path) as f:
+    return f.read()
 
 def word_count(text):
   return len(text.split())
+
+def char_count(text):
+  char_count = {}
+
+  for char in text.lower():
+    if char in char_count:
+      char_count[char] += 1
+    else:
+      char_count[char] = 1
+
+  return char_count
 
 main()
